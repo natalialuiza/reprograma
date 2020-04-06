bc91afdffdcdfc23e1329cdceaafb222a6a723e6
Natalia Luiza <natalialuizas@live.com>
2020-04-06 13:17:02 -0300

    semana 05

---
+++ semana-05/aula-01/app.js
@@ -0,0 +1,24 @@
+// exercicio 01
+alert('Hello Wolrd');
+
+// exercicio 02
+const value01 = parseFloat(prompt('informe o valor 1'));
+const value02 = parseFloat(prompt('informe o valor 2'));
+alert( value01 + value02);
+
+
+// exercicio 03
+const nome = prompt('Informe o seu nome');
+const endereco = prompt ('informe seu endereco');
+const telefone = prompt('informe o seu telefone');
+
+// exercicio 04
+const anoNascimento = parseFloat(prompt(`informe a sua data de Nascimento`));
+alert(`Sua idade e ${2020 - anoNascimento}`);
+
+// exercicio 05
+const value1 = parseFloat(prompt('informe o valor 1'));
+const value2 = parseFloat(prompt('informe o valor 2'));
+const value3 = parseFloat(prompt('informe o valor 3'));
+
+alert(`a media dos tres valores e ${(value1 + value2 + value3) / 3}`)
