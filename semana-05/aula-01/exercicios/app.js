7b6fe143bcbb29fc4749f93392b905413b3fb1a0
Natalia Luiza <natalialuizas@live.com>
2020-04-06 16:53:04 -0300

    aula 01 de logica com js ok

--- semana-05/aula-01/exercicios/app.js
+++ semana-05/aula-01/exercicios/app.js
@@ -1,24 +1,24 @@
-// exercicio 01
+// 1. Faça uma mensagem de "Olá Mundo!";
 alert('Hello Wolrd');
 
-// exercicio 02
-const value01 = parseFloat(prompt('informe o valor 1'));
-const value02 = parseFloat(prompt('informe o valor 2'));
-alert( value01 + value02);
+// 2. Faça uma saída de soma de 2 números;
+alert( 10 + 85);
 
 
-// exercicio 03
+// 3. Crie um algoritmo que receba nome, endereço e telefone, e depois mostre-os;
 const nome = prompt('Informe o seu nome');
 const endereco = prompt ('informe seu endereco');
 const telefone = prompt('informe o seu telefone');
 
-// exercicio 04
+// 4. Solicite o ano de nascimento do usuário, calcule sua idade e mostre-a;
 const anoNascimento = parseFloat(prompt(`informe a sua data de Nascimento`));
 alert(`Sua idade e ${2020 - anoNascimento}`);
 
-// exercicio 05
+// 5. Receba do usuários 3 números e calcule a média entre eles.
 const value1 = parseFloat(prompt('informe o valor 1'));
 const value2 = parseFloat(prompt('informe o valor 2'));
 const value3 = parseFloat(prompt('informe o valor 3'));
 
 alert(`a media dos tres valores e ${(value1 + value2 + value3) / 3}`)
+
+
---
+++ semana-05/aula-02/exercicios/app.js
@@ -0,0 +1,32 @@
+// 1. Crie um algoritmo que dado um valor, calcule 5% de desconto e retorne o valor do desconto;
+
+const valor = parseFloat(prompt('Informe o valor'));
+alert(`O valor do desconto ${valor * 5  / 100 }`)
+
+// 2. Crie um algoritmo que receba dois valores e exiba o resultado e o resto da divisão entre eles;
+const num1 = parseFloat(prompt('Informe o valor 1'));
+const num2 = parseFloat(prompt('Informe o valor 2'));
+alert(`A soma de ${num1} + ${num2} = ${num1 + num2}`)
+
+// 3. Crie um algoritmo que calcule quantos dias o usuário viveu, baseado na idade (considere que a pessoa tenha a idade exata);
+const idade = parseInt(prompt('Informe a sua idade'));
+
+alert(`Quantos dias voce viveu ${idade * 365}`)
+
+// 4. Faça um algoritmo que leia a idade de uma pessoa em dias e mostre-a em anos, meses e dias (assuma que todos os meses tem 30 dias, e todos os anos 365 dias);
+const idadeEmDias = parseInt(prompt('Informe a sua idade em dias'));
+
+let idadePessoa = parseInt(idadeEmDias / 365);
+let idadeMes = parseInt((idadeEmDias % 365) / 30);
+let idadeDias = parseInt((idadeMes % 30) / 30);
+
+alert(`Sua idade e ${idadePessoa}, ${idadeMes} meses,  ${idadeDias} dias`);
+
+
+// (TODAS AS QUESTÕES ABAIXO SÃO DE UM MUNDO SEM COVID-19. TODO MUNDO TAVA NA RUA. VOCÊS FIQUEM EM CASA)
+
+// 5. Wenceslay gostaria de saber quantos litros por quilômetro, sua moto gastou em média. Ele sabe a distância rodada e a quantidade de litros de combustível que ele utilizou. Crie um algoritmo que mostre a Wenceslay qual a média de combustível gasto por quilômetro.
+
+// 6. O gerente ficou louco! E agora não sabe quanto de desconto deu nos seus produtos para fazer o anúncio. Ele sabe o preço sem o desconto e com o desconto. Faça um algoritmo que calcule o percentual de desconto do produto.
+
+// 7. Zenir é responsável por colocar a duração dos filmes no letreiro do cinema comunitário. A duração deve ser mostrada em minutos, mas alguns dos filmes exibidos vieram com a informação em horas e minutos. Ajude Zenir, ela tem mais o que fazer do que ficar fazendo conta.
