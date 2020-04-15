da06872711bb512b5d25856dd2a5b94310b6ae68
Natalia Luiza <natalialuizas@live.com>
2020-04-15 15:20:15 -0300

    exercicios de objeto e array da aula 03 de JS I

---
+++ semana-06/aula-03/exercicios/objeto.js
@@ -0,0 +1,28 @@
+// 1. Crie um Objeto pessoa que receba as propriedades: nome, telefone, idade, altura e peso.
+
+const pessoa = {
+    nome: 'Natália Luiza',
+    telefone: '17-999999999',
+    idade: 24, 
+    altura: 1.65,
+    peso: 85
+}
+console.log(pessoa);
+// 2. Crie um objeto animal que receba as propriedades: porte(pequeno, médio ou grande), especie, raça.
+
+const animal = {
+    porte: 'Pequeno',
+    especie: 'mamífero carnívoro',
+    raça: 'vira-lata'
+}
+console.log(animal);
+
+// 3. Crie um objeto curso que receba as propriedades: nome, período, professor(a).  
+
+const curso = {
+    nome: 'Bootcamp - FrontEnd',
+    periodo: 'integral',
+    professora: 'bruna'
+}
+console.log(curso);
+
