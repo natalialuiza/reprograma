10beba62ce6e647fe436aa910c5cdd1b6dd5c2bb
Natalia Luiza <natalialuizas@live.com>
2020-04-17 16:28:20 -0300

    adicionando o exercicio 05

--- semana-06/aula-05/for.js
+++ semana-06/aula-05/for.js
@@ -37,3 +37,27 @@ for(let i=0; i < 5; i++){
   arrayDeNomes.push(nome);
 }
 
+
+//5. Faça um programa que receba 10 números, 
+//calcule e imprima a soma dos números pares e a soma dos números ímpares.
+
+// - for para ler 10 prompt 
+let numero = 0, somaImpares = 0, somaPares = 0,  contPar = 0, contImpar = 0;
+
+for (let num = 1; num <= 10; num++) {
+    numero = parseInt(prompt(`Digite o número ${num} :`));
+
+    if (numero % 2 === 0) {
+        contPar++;
+        somaPares += numero;
+    }
+    else {
+        contImpar++; 
+        somaImpares += numero;
+    }
+}
+
+console.log(somaImpares, 'soma de numero impares');
+console.log(somaPares, 'soma de numeros pares');
+
+
