24c2d191c81fc997dad64fc5525bc11e14e7329f
Natalia Luiza <natalialuizas@live.com>
2020-05-27 11:08:03 -0300

    exercicios do dia 01 de JS III

--- semana-12/01-arrays/script.js
+++ semana-12/01-arrays/script.js
@@ -2,11 +2,49 @@
 // 1) Adicionar "a" no final das palavras
 const profissionais = ["desenvolvedor", "programador", "autor", "vereador", "professor"];
 
+const profissionaisMelhores = profissionais.map( item => item + 'a');
 
 
 // 2) Retornar frutas com somente a primeira letra maiúscula
 const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
 
+const novaFrutas = frutas.map(item => item[0].toUpperCase() + item.slice(1).toLowerCase()
+)
+
+// 3)
+
+const notasAlunas = [
+  { nome: "Julia", notas: [
+      { materia: "matemática", nota: 7, peso: 1.5 },
+      { materia: "portugues", nota: 3, peso: 2 }
+    ]
+  },
+  { nome: "Zelia", notas: [
+      { materia: "matemática", nota: 6, peso: 1.5 },
+      { materia: "portugues", nota: 5, peso: 2 }
+    ]
+  },
+  { nome: "Jussara", notas: [
+      { materia: "matemática", nota: 8, peso: 1.5 },
+      { materia: "portugues", nota: 2, peso: 2 }
+    ]
+  }
+]
+
+const notasAlunasFinais = notasAlunas.map(({ nome, notas}) => {
+  let somaNotas = 0;
+  let somaPesos = 0
+  
+    for(let i =0; i< notas.length; i++){
+      const nota = notas[i].nota;
+      const peso = notas[i].peso;
+      somaNotas += nota * peso
+      somaPesos += peso;
+    }
+    let notaFinal = somaNotas / somaPesos
+
+   return { nome, notaFinal}
+})
 
 
 
@@ -15,6 +53,7 @@ const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
 // 1) Criar nova array somente com números pares
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const pares = num.filter(numero => numero % 2 === 0)
 
 
 // 2) Criar nova array somente com profissionais da área de TI
@@ -26,12 +65,22 @@ const pessoas = [
   { nome: "Monica", area: "medicina", profissao: "cardiologista"},
 ];
 
+const profissionaisTI = pessoas.filter(pessoa => pessoa.area === "TI")
+
+// 3) Criar um array somente com alunas que passaram de ano (media > 5)
+
+const alunasPassantes = notasFinaisAlunas.filter(({ notaFinal }) => notaFinal > 5)
 
 
 
 // reduce(): recebe uma array e retorna um valor só. Redutor.
 
 // 1) Dado uma array de números, retornar a soma de todos eles
+const newNumbers = [1,2,3,4,5]
+
+const reduce = newNumbers.reduce((accumulator, atual) => {
+  return accumulator += atual
+}, 0)
 
 
 // 2) Retornar o lucro de uma empresa
@@ -42,11 +91,25 @@ const transacoes = [
   { descricao: "salario 1", valor: 8500, tipo: "saída" }
 ];
 
+const lucro = transacoes.reduce((accumulator, { tipo, valor})  => {
+  if (tipo === 'entrada') {
+    return accumulator += valor;
+  }
+  return accumulator;
+}, 0)
+
 
 // sort(): organiza itens da array de acordo com uma verificação
 
 // 1) Organizar números de forma decrescente
+const numbers = [10,30,68,95,45,96,100,22]
+
+numbers.sort()
 
 
 // 2) Organizar pessoas por ordem alfabética
+
+const newPessoas = [ 'Natalia', 'Brenda', 'Ana Laura', 'Geovanna', 'Isabel', 'Ester']
+
+newPessoas.sort()
 
