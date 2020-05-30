7914e3297670555f312dd41a0db640e2c50c7a46
Natalia Luiza <natalialuizas@live.com>
2020-05-30 11:49:29 -0300

    exercicio do tarot resolvido

---
+++ semana-12/04-fetch/exercicio/exercicio-01/js/script.js
@@ -0,0 +1,40 @@
+const cardSection = document.getElementById('cards-section')
+const errorSection = document.getElementById('error')
+const loading = document.getElementById('spinner')
+const btn = document.getElementById('btn-novaCarta')
+// Caso os cards não carreguem, mostraremos isso:
+const placeholderCard = {
+  nome: "Carta não carregada",
+  tipo: "Arcano Maior",
+  descricao: "Sem descrição",
+  imagem: "http://via.placeholder.com/250X500",
+  link: "https://www.astrolink.com.br/"
+}
+
+const renderizaCarta = carta => {
+  const novaCarta = new Card(carta).render()
+  cardSection.innerHTML = novaCarta 
+}
+
+const randomCard = cartas => cartas[(Math.floor(Math.random() * cartas.length))]
+
+
+btn.addEventListener('click', () => {
+  fetch('https://raw.githubusercontent.com/reprograma/T9-JS-III/final/04-fetch/exercicio/tarot.json')
+  .then(dadosCartas => dadosCartas.json())
+  .then(cartas => {
+    let cartaSorteada = randomCard(cartas)
+    renderizaCarta(cartaSorteada)
+  })
+})
+
+renderizaCarta(placeholderCard)
+
+
+
+
+
+
+
+
+
